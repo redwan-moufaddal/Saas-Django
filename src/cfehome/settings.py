15af60e2c9a290200d374a4ac7a6269d2b166b0f
RedwanMoufaddal <ridouanmoufaddal1@gmail.com>
2024-07-26 11:54:32 +0100

    setting static

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# The directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Additional directories where Django will look for static files.
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
